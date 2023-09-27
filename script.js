aaf124c94e6671378ae39308a3ad3c422087ffa6
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-27 18:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+// Get references to the select element and the remove button
+const colorSelect = document.getElementById("colorSelect");
+const removeButton = document.getElementById("removeButton");
+
+// Add a click event listener to the remove button
+removeButton.addEventListener("click", function() {
+    const selectedIndex = colorSelect.selectedIndex;
+    
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    }
+});
